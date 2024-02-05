f4620dbc761553f604e5c7dc96155e49c48fd82a
SebastianVergaraVelasquez <97114145+SebastianVergaraVelasquez@users.noreply.github.com>
2024-02-04 22:41:58 -0500

    feat: :sparkles: Modal factura
    Se hizo un ajuste al archivo main.js para desplegar una ventana modal
    al pulsar el carrito, fue ajustada para escritorio y movil. Por ahora
    solo funciona en la primera categoria.
    
    commit c13f08f0219a494927b03132fab3dafbdd2da04d

--- js/main.js
+++ js/main.js
@@ -12,3 +12,18 @@ btnAbrirModal.addEventListener("click",() =>{
 btnCerrarModal.addEventListener("click",()=>{
     modal.close();
 })
+
+const btnAbrirModalFactura =
+document.querySelector("#btn-abrir-modal-factura");
+const btnCerrarModalFactura =
+document.querySelector("#btn-cerrar-modal-factura");
+const modalFactura =
+document.querySelector("#modal-factura");
+
+btnAbrirModalFactura.addEventListener("click",() =>{
+    modalFactura.showModal();
+})
+
+btnCerrarModalFactura.addEventListener("click",()=>{
+    modalFactura.close();
+})
